72890eb82ebcbe989a704b87172fee95980a3eaf
Akshat Tandon <tandonakshat7@gmail.com>
2017-04-04 11:45:37 +0200

    Introduced error handling

--- 02_Build/scrapper.py
+++ 02_Build/scrapper.py
@@ -4,18 +4,22 @@ import requests
 
 def file_download(file_url):
     local_filename = file_url.split('/')[-1]
-    print(local_filename)
     r = requests.get(file_url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-                f.flush()
-    return local_filename
-
-fileUrl = "http://users.fs.cvut.cz/~tandoaks/test.docx"
-
-test = file_download(fileUrl)
-
+    if r.status_code == 200:
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        return local_filename
+        pass
+    return False
 
 
+test_takers = ["tandoaks"]
+
+for test_taker in test_takers:
+    fileUrl = "http://users.fs.cvut.cz/~" + test_taker + "/test.doc"
+    test = file_download(fileUrl)
+    if file_download(fileUrl) is False:
+        print("Error while downloading files from server.")
