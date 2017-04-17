0ef61314e27f7bec182859665b53501a259ae64c
Akshat Tandon <tandonakshat7@gmail.com>
2017-04-17 17:35:05 +0200

    Introticed success report generation

--- 02_Build/scrapper.py
+++ 02_Build/scrapper.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import requests
 import os
+import errno
 import easygui
 import logging
 
@@ -38,28 +39,58 @@ def test_takers():
         test_takers_list = open_file.readlines()
         if open_file:
             open_file.close()
-        return test_takers_list
+        test_takers_list_ref = []
+        for i in range(len(test_takers_list)):
+            if test_takers_list[i].endswith("\n") and test_takers_list[i] != "\n" and test_takers_list[i] != "":
+                test_takers_list_ref.append(test_takers_list[i][:-1])
+        return test_takers_list_ref
+
     except Exception as e:
         print(e.message)
         raise
 
 
 def main():
-    tasks_folders = ["Task1"]
+    """Entry point to the script
+
+    This function runs and generates report on how
+    successful the script ran.
+    """
+    import datetime
+
+    tasks_folders = ["Task1", "Task2", "Task3"]
     test_takers_list = test_takers()
     print(test_takers_list)
-    print(" ___________" + "__________________________________________")
-    print("| Test Taker " + "|                  Status                |")
-    for test_taker in test_takers_list:
-        fileurl = "http://users.fs.cvut.cz/~" + test_taker + "/test.docx"
-        if file_download(fileurl):
-            for tasks_folder in tasks_folders:
-                # fileUrl = "http://users.fs.cvut.cz/~" + test_taker + "/"+ tasks_folders "/test.docx"
-                test = file_download(fileurl)
-                print("|  " + test_taker + "  |      Files successfully downloaded     |")
-        else:
-            print("|  " + test_taker + "  |Error while downloading files from server|")
 
+    curr_dir = os.getcwd()
+    rep_dir = os.path.join(curr_dir, "Reports")
+    if not os.path.exists(rep_dir):
+        os.mkdir(rep_dir)
+
+    with open(os.path.join("Reports", "report " + str(datetime.datetime.now()) + ".txt"), "w") as report_file:
+        report_file.write(" " + "_"*64 + "\n")
+        report_file.write("| Test Taker " + "| Tasks " + "  |                  Status                 |" + "\n")
+        report_file.write(" " + "-"*64 + "\n")
+        for test_taker in test_takers_list:
+            count = 0
+            for tasks_folder in tasks_folders:
+                count += 1
+                # fileurl = "http://users.fs.cvut.cz/~" + test_taker + "/" + tasks_folder + "/test.docx"
+                # fileurl = "http://users.fs.cvut.cz/~" + test_taker + "/test.docx"
+                fileurl = "http://users.fs.cvut.cz/~" + test_taker
+                if file_download(fileurl):
+                    test = file_download(fileurl)
+                    if count == 1:
+                        report_file.write("|  " + test_taker + "  | " + tasks_folder + "   |      Files successfully downloaded      |" + "\n")
+                    else:
+                        report_file.write("|            | " + tasks_folder + "   |      Files successfully downloaded      |" + "\n")
+                else:
+                    if count == 1:
+                        report_file.write("|  " + test_taker + "  | " + tasks_folder + "   |Error while downloading files from server|" + "\n")
+                    else:
+                        report_file.write("|            | " + tasks_folder + "   |Error while downloading files from server|" + "\n")
+        report_file.write(" " + "-"*64 + "\n")
+        report_file.close()
 
 if __name__ == "__main__":
     main()
